fe48cc8a8ea4d2818142ae1b24231d1a6b7b84c5
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2020-03-27 13:54:14 +0100

    Bump version of DO TF provider to fix bug with cache

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 0.12"
   required_providers {
-    digitalocean = "~> 1.13"
+    digitalocean = "~> 1.15.1"
     local        = "~> 1.4"
     template     = "~> 2.1"
   }
